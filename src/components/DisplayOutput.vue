aee954832c906fe56b4abe68dd0e2579b06fc47d
Luke Pashley <luke.pashley@icabbi.com>
2023-07-10 21:51:17 +0100

    Added new syntax highlight + debug toggle

--- src/components/DisplayOutput.vue
+++ src/components/DisplayOutput.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <!-- <textarea v-model="markdown"></textarea> -->
-    <lili-cont title="Output">
+    <lili-cont title="Debug" v-if="showDebug">
+      <textarea v-model="markdown"></textarea>
+      <q-btn @click="switchHighlightEngine" color="green">Switch Highlight Engine</q-btn>
+      Engine: {{ highlightEngine }}
+    </lili-cont>
+    <lili-cont :title="true">
+      <template v-slot:title>
+        <div class="row items-center">
+          <div class="col">Output</div>
+          <div class="col-shrink">
+            <q-btn size="10px" @click="showDebug = !showDebug" outline color="primary">Debug</q-btn>
+          </div>
+        </div>
+      </template>
       <div v-html="markdownToHtml"></div>
     </lili-cont>
   </div>
@@ -9,7 +21,14 @@
 
 
 <script>
+// Markdown Parser
 import { marked } from 'marked';
+
+// Syntax Highlighting (highlight.js)
+import hljs from 'highlight.js';
+import "highlight.js/styles/github.css"
+
+// Syntax Highlighting (prism.js)
 import prism from 'prismjs';
 import "prismjs/themes/prism.css"; // you can change
 
@@ -27,6 +46,8 @@ import 'prismjs/components/prism-scss';
 export default {
   data() {
     return {
+      highlightEngine: 'highlightjs',
+      showDebug: false,
     //   markdown: " # hello world \n ```js\n function hello(){ console.log(Hello World) } \n```",
     };
   },
@@ -42,9 +63,25 @@ export default {
       return marked.parse(this.markdown);
     }
   },
+  methods: {
+    switchHighlightEngine(){
+      if(this.highlightEngine === 'prism'){
+        this.highlightEngine = 'highlightjs';
+      } else {
+        this.highlightEngine = 'prism';
+      }
+    }
+  },
   beforeMount() {
     marked.setOptions({
       highlight: (code, lang) => {
+        console.log('highlight', code, lang)
+        if (this.highlightEngine === 'highlightjs') {
+          const langObj = hljs.getLanguage(lang) ? [lang] : undefined;
+          console.log('langObj', langObj)
+          return hljs.highlightAuto(code, langObj).value;
+        }
+        
         if (prism.languages[lang]) {
           return prism.highlight(code, prism.languages[lang], lang);
         } else {
