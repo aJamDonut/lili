e662b66b7f371c6022417a6e9c2dd13612f08a77
Luke Pashley <luke.pashley@icabbi.com>
2023-07-13 02:34:26 +0100

    Added timeout to save storage

--- src/boot/settings_storage.ts
+++ src/boot/settings_storage.ts
@@ -6,11 +6,23 @@ const settings = useSettingsStore();
 
 console.log('settings Storage', settings.$state)
 
+type TimerId = NodeJS.Timeout | null
+
+let timerId: TimerId = null;
+
 watch(
   settings.$state,
   async (state) => {
-    const storage = new Storage();
-    await storage.writeFile('config', 'settings.json', JSON.stringify(state));
+    // Add sleep timer to prevent too many writes
+    // Cancel and create new timer on each change
+    if (timerId) clearTimeout(timerId)
+    timerId = setTimeout(async () => {
+      console.log('settings Storage write', state)
+      const storage = new Storage();
+      await storage.writeFile('config', 'settings.json', JSON.stringify(state));
+    }, 500);
+    // const storage = new Storage();
+    // await storage.writeFile('config', 'settings.json', JSON.stringify(state));
   },
   { deep: true }
   );
