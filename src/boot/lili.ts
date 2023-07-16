af71e098f89f68afddbd84e9bcd3cdb0301aeb42
Adam Dougherty <adamiandougherty@gmail.com>
2023-07-16 21:33:34 +0100

    add notifications

--- src/boot/lili.ts
+++ src/boot/lili.ts
@@ -1,8 +1,38 @@
+import { Notify } from 'quasar';
 import { boot } from 'quasar/wrappers';
 import { useWorkloadsStore } from 'src/stores/workloads';
+import { on } from '../services/electron';
 
 const workloads = useWorkloadsStore();
 
+interface LiliNotification {
+  status: 'warning' | 'information' | 'error';
+  content: string;
+  linkLabel?: string;
+  linkUrl?: string;
+}
+
+const colors = {
+  warning: 'yellow',
+  information: 'yellow',
+  error: 'red',
+};
+
 export default boot(({ app }) => {
   workloads.load();
+
+  const handler = () => {
+    this.$router.push();
+  };
+
+  on('LiliEngine:notify', (message: LiliNotification) => {
+    const actions = !message.linkLabel ? [] : [{ label: message.linkLabel, handler }];
+    Notify.create({
+      group: false,
+      message: message.content,
+      color: colors[message.status],
+      position: 'bottom-right',
+      actions: actions,
+    });
+  });
 });
