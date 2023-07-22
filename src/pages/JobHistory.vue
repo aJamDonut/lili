75a29f7dcd1de87f4a16f52d0e4d73afb6f623ec
Adam Dougherty <adamiandougherty@gmail.com>
2023-07-22 17:04:29 +0100

    some tweaks and fixes

--- src/pages/JobHistory.vue
+++ src/pages/JobHistory.vue
@@ -2,13 +2,19 @@
   <q-page padding>
     <div :class="lockedPageClass">
       <h1 class="page-title">{{ pageTitle }}</h1>
+      <p>Workloads are repeatable conversations which allow you to quickly perform repeatable actions</p>
       <q-table :rows="jobStore.jobHistory" :pagination="pagination" :columns="columns" row-key="id" flat bordered separator="cell">
         <template v-slot:body="props">
           <q-tr :props="props">
             <q-td :props="props" v-for="row in props.cols" :key="row.name">
               <div class="row items-center justify-center q-col-gutter-sm" style="min-width: 180px" v-if="row.name === 'actions'">
                 <div>
-                  <q-btn outline color="primary" size="11px" icon="replay" :to="{ path: '/job/' + props.row.meta.id }" />
+                  <div v-if="type === 'history'">
+                    <q-btn outline color="primary" size="11px" icon="send" :to="{ path: '/job/history/' + props.row.meta.id }" />
+                  </div>
+                  <div v-else>
+                    <q-btn outline color="primary" size="11px" icon="send" :to="{ path: '/job/workload/' + props.row.meta.id }" />
+                  </div>
                 </div>
                 <div>
                   <q-btn outline color="orange-8" size="11px" icon="tune" :to="{ path: '/edit/' + props.row.meta.id }" />
@@ -35,39 +41,39 @@
       </q-table>
 
       <div v-if="table2 !== false" class="q-mt-lg">
-      <h1 class="page-title">Table2</h1>
-      <q-table :rows="table2" :pagination="pagination" :columns="columns" row-key="id" flat bordered separator="cell">
-        <template v-slot:body="props">
-          <q-tr :props="props">
-            <q-td :props="props" v-for="row in props.cols" :key="row.name">
-              <div class="row items-center justify-center q-col-gutter-sm" style="min-width: 180px" v-if="row.name === 'actions'">
-                <div>
-                  <q-btn outline color="primary" size="11px" icon="replay" :to="{ path: '/job/' + props.row.meta.id }" />
+        <h1 class="page-title">Lili Workloads</h1>
+        <q-table :rows="table2" :pagination="pagination" :columns="columns" row-key="id" flat bordered separator="cell">
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td :props="props" v-for="row in props.cols" :key="row.name">
+                <div class="row items-center justify-center q-col-gutter-sm" style="min-width: 180px" v-if="row.name === 'actions'">
+                  <div>
+                    <q-btn outline color="primary" size="11px" icon="replay" :to="{ path: '/job/' + props.row.meta.id }" />
+                  </div>
+                  <div>
+                    <q-btn outline color="orange-8" size="11px" icon="tune" :to="{ path: '/edit/' + props.row.meta.id }" />
+                  </div>
+                  <div>
+                    <q-btn outline color="red" size="11px" icon="close" @click="deleteJob(props.row.meta.id)" />
+                  </div>
                 </div>
-                <div>
-                  <q-btn outline color="orange-8" size="11px" icon="tune" :to="{ path: '/edit/' + props.row.meta.id }" />
+                <div v-else-if="row.name === 'id'">
+                  {{ row.value }}
                 </div>
-                <div>
-                  <q-btn outline color="red" size="11px" icon="close" @click="deleteJob(props.row.meta.id)" />
+                <div v-else-if="row.name === 'status'">
+                  <q-chip size="10px" color="green" text-color="white"> COMPLETED </q-chip>
                 </div>
-              </div>
-              <div v-else-if="row.name === 'id'">
-                {{ row.value }}
-              </div>
-              <div v-else-if="row.name === 'status'">
-                <q-chip size="10px" color="green" text-color="white"> COMPLETED </q-chip>
-              </div>
-              <div class="truncate" v-else>{{ row.value }}</div>
-            </q-td>
-          </q-tr>
-        </template>
-        <template v-slot:no-data>
-          <div class="full-width row flex-center q-my-lg">
-            <div class="text-grey-6">{{ $t('no_job_history') }}</div>
-          </div>
-        </template>
-      </q-table>
-    </div>
+                <div class="truncate" v-else>{{ row.value }}</div>
+              </q-td>
+            </q-tr>
+          </template>
+          <template v-slot:no-data>
+            <div class="full-width row flex-center q-my-lg">
+              <div class="text-grey-6">{{ $t('no_job_history') }}</div>
+            </div>
+          </template>
+        </q-table>
+      </div>
     </div>
     <locked-overlay />
   </q-page>
@@ -82,6 +88,7 @@ import { useSettingsStore } from 'stores/settings';
 export default {
   data() {
     return {
+      type: this.$route.params.type || 'history',
       displayOutput: false,
       markdown: ' # hello world \n ```js\n function hello(){ console.log(Hello World) } \n```',
       table2: false,
@@ -97,35 +104,75 @@ export default {
     lockedPageClass() {
       return this.settingsStore.isValidKey ? '' : 'locked-page';
     },
-    pageTitle () {
+    pageTitle() {
       if (this.$route.params.type === 'history') {
         return this.$t('job_history');
       } else {
-        return this.$t('workloads');
+        return this.$t('your_workloads');
       }
     },
     columns() {
-
       let cols = [
         { name: 'actions', label: this.$t('actions'), field: 'actions', align: 'center', sortable: false, headerClasses: 'q-table--col-auto-width' },
-      ]
+      ];
 
       if (this.$route.params.type === 'history') {
-        cols.push({ name: 'id', label: this.$t('id'), field: (r) => r.meta.id, align: 'left', sortable: true, headerClasses: 'q-table--col-auto-width' })
-        cols.push({ name: 'prompt', label: this.$t('prompt'), field: (r) => r.workloadOptions.prompt, align: 'left', sortable: true })
-        cols.push({ name: 'workload', label: this.$t('workload'), field: (r) => r.workloadDefinition.name, align: 'left', sortable: true, headerClasses: 'q-table--col-auto-width' })
-        cols.push({ name: 'status', label: this.$t('status'), field: 'status', align: 'left', sortable: true, headerClasses: 'q-table--col-auto-width' })
+        cols.push({
+          name: 'id',
+          label: this.$t('id'),
+          field: (r) => r.meta.id,
+          align: 'left',
+          sortable: true,
+          headerClasses: 'q-table--col-auto-width',
+        });
+        cols.push({ name: 'prompt', label: this.$t('prompt'), field: (r) => r.workloadOptions.prompt, align: 'left', sortable: true });
+        cols.push({
+          name: 'workload',
+          label: this.$t('workload'),
+          field: (r) => r.workloadDefinition.name,
+          align: 'left',
+          sortable: true,
+          headerClasses: 'q-table--col-auto-width',
+        });
+        cols.push({
+          name: 'status',
+          label: this.$t('status'),
+          field: 'status',
+          align: 'left',
+          sortable: true,
+          headerClasses: 'q-table--col-auto-width',
+        });
       } else {
-        cols.push({ name: 'name', label: this.$t('name'), field: (r) => r.workloadDefinition.name, align: 'left', sortable: true, headerClasses: 'q-table--col-auto-width' })
-        cols.push({ name: 'description', label: this.$t('description'), field: (r) => r.workloadDefinition.description, align: 'left', sortable: true })
-        cols.push({ name: 'codename', label: this.$t('codename'), field: (r) => r.workloadDefinition.codename, align: 'left', sortable: true, headerClasses: 'q-table--col-auto-width' })
+        cols.push({
+          name: 'name',
+          label: this.$t('name'),
+          field: (r) => r.workloadDefinition.name,
+          align: 'left',
+          sortable: true,
+          headerClasses: 'q-table--col-auto-width',
+        });
+        cols.push({
+          name: 'description',
+          label: this.$t('description'),
+          field: (r) => r.workloadDefinition.description,
+          align: 'left',
+          sortable: true,
+        });
+        cols.push({
+          name: 'codename',
+          label: this.$t('codename'),
+          field: (r) => r.workloadDefinition.codename,
+          align: 'left',
+          sortable: true,
+          headerClasses: 'q-table--col-auto-width',
+        });
       }
 
-      return cols
+      return cols;
     },
   },
   methods: {
-    async load () {
+    async load() {
       this.table2 = false;
       this.jobStore.empty();
       if (this.$route.params.type === 'user') this.table2 = await this.jobStore.getHistory(0, 100, 'core');
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,18 +6,15 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/job/:id?', component: () => import('pages/ViewJob.vue') },
+      { path: '/job/:type/:id?', component: () => import('pages/ViewJob.vue') },
       { path: '/edit/:id?', component: () => import('pages/JobDetail.vue') },
       { path: '/history/:type?', component: () => import('pages/JobHistory.vue') },
       { path: '/settings', component: () => import('src/pages/UserSettings.vue') },
+      {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/ErrorNotFound.vue'),
+      },
     ],
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
   },
 ];
 
