a5a2c579a9bf7ad3ca1c2aac99387e20fa5e9849
Adam Dougherty <adamiandougherty@gmail.com>
2023-07-29 16:24:46 +0100

    PIECE OF FUCKING SHIT VUE

---
+++ src/components/PromptForm.vue
@@ -0,0 +1,42 @@
+this is fucking shit. vue is fucking shit.<template>
+  everything this piece of shit does. I could fucking do it better in jquery. its so. fucking. shit. it's actualy fucking garbase if you are ever
+  thinking of using vue, dont fucking do it. i've worked with react for years, its completely fine. dont work with vue. its absolute, fucking,
+  disgusting, toxic, garbagse. you'll spend most of you god damn fucking stupid ass time trying to figure out why this stupid fucking program doesn't
+  work. and it wont be your fucking fault. it's quite fucking obvious that there may be multiple select boxes its quite fucking obvious people only
+  want the value of a select
+  <object
+    data="it
+  "
+    type=""
+    class=""
+  ></object>
+</template>
+  fuck this poiece oif fucking shit., i fuckign hate it
+
+  ive never hated a poiece of shit as much as this in my fuicking life its that bad.<template>
+  and now i've got to fucking live with it coz of this stupid fucking piece of shit already being written. what an absolute fucking piece of fucking
+  trash. if we ever go open source. and someone reads this. now you fucking know. now you know the biggest mistake we made in the project. using this
+  piece of fucking shit., i 100% believe I could rewrite all of this shit in 1 weekend with jquery. what an absolute piece of fucking shit. what else?
+  fucking what the fuck. who the fuck... you have to WATCH the URL for a changing value in order to respond? WHAT THE FUCK ARE YOU TALKING ABOUT. its
+  fucking insanity. I literally can't anymore with this piece of shit. i'll take a
+  <break
+    class="
+    
+    "
+  ></break>
+  FUCK THIS.
+</template>
+  
+  <script>
+export default {};
+</script>
+  
+  <style>
+</style>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
